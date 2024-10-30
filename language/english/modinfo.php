f6411ff0cd9bbeee2b7ca8b2e6ecdcfc5e0b5a15
GregMage <gregory.mage@gmail.com>
2024-10-30 17:56:13 +0100

    création de la base du module

---
+++ language/english/modinfo.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * xmstats module
+ *
+ * @copyright       XOOPS Project (https://xoops.org)
+ * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author          Mage Gregory (AKA Mage)
+ */
+// The name of this module
+define('_MI_XMSTATS_DESC', 'Module de statistiques pour les modules de la famille XM');
+
+// Menu
+define('_MI_XMSTATS_MENU_HOME', 'Index');
+define('_MI_XMSTATS_MENU_PERMISSION', 'Permission');
+define('_MI_XMSTATS_MENU_ABOUT', 'About');
+
+// Pref
+define('_MI_XMSTATS_PREF_HEAD_GENERAL', '<span style="font-size: large;  font-weight: bold;">--- General ---</span>');
+define('_MI_XMSTATS_PREF_GENERALITEMPERPAGE', 'Number of items per page in the general view');
---
+++ xoops_version.php
@@ -0,0 +1,87 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * xmstats module
+ *
+ * @copyright       XOOPS Project (https://xoops.org)
+ * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author          Mage Gregory (AKA Mage)
+ */
+$modversion['dirname']     = basename(__DIR__);
+$modversion['name']        = ucfirst(basename(__DIR__));
+$modversion['version']     = '0.0.1-Alpha';
+$modversion['description'] = _MI_XMSTATS_DESC;
+$modversion['author']      = 'Grégory Mage (Mage)';
+$modversion['url']         = 'https://github.com/GregMage';
+$modversion['credits']     = 'Mage';
+
+$modversion['help']        = 'page=help';
+$modversion['license']     = 'GNU GPL 2 or later';
+$modversion['license_url'] = 'http://www.gnu.org/licenses/gpl-2.0.html';
+$modversion['official']    = 0;
+$modversion['image']       = 'assets/images/xmstats_logo.png';
+
+// Menu
+$modversion['hasMain'] = 1;
+
+// Admin things
+$modversion['hasAdmin']    = 1;
+$modversion['system_menu'] = 1;
+$modversion['adminindex']  = 'admin/index.php';
+$modversion['adminmenu']   = 'admin/menu.php';
+
+// Install and update
+
+
+// Tables
+
+
+// Admin Templates
+$modversion['templates'][] = ['file' => 'xmstats_admin_permission.tpl', 'description' => '', 'type' => 'admin'];
+
+// User Templates
+$modversion['templates'][] = ['file' => 'xmstats_index.tpl', 'description' => ''];
+
+// Blocks
+
+
+// Configs
+$modversion['config'] = [];
+
+$modversion['config'][] = [
+    'name'        => 'break',
+    'title'       => '_MI_XMSTATS_PREF_HEAD_GENERAL',
+    'description' => '',
+    'formtype'    => 'line_break',
+    'valuetype'   => 'text',
+    'default'     => 'head',
+];
+
+$modversion['config'][] = [
+    'name'        => 'general_perpage',
+    'title'       => '_MI_XMSTATS_PREF_GENERALITEMPERPAGE',
+    'description' => '',
+    'formtype'    => 'textbox',
+    'valuetype'   => 'int',
+    'default'     => 15
+];
+
+
+// About stuff
+$modversion['release_date']  = '2024/10/30';
+
+$modversion['developer_lead']      = 'Mage';
+$modversion['module_website_url']  = 'github.com/GregMage';
+$modversion['module_website_name'] = 'github.com/GregMage';
+
+$modversion['min_xoops'] = '2.5.11';
+$modversion['min_php']   = '8.0';
